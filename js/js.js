f89f28216f80c6bc0201b180d3a536a93616ba32
cdmngz <crlsdmngz3@gmail.com>
2013-09-29 23:15:49 -0300

    Starting
    Updating files from localhost

--- js/js.js
+++ js/js.js
@@ -1,4 +1,33 @@
-slide
-=====
+$(function(){
+	var sliderAct=1;
+	var sliderNext=2;
 
-Testing fadeIn, fadeOut functions in a simple slider
+	$('#slider > div#slide1').fadeIn(300);
+	startSlider();
+
+	function startSlider(){
+		count = $('#slider > div').size();
+
+		loop = setInterval(function(){
+			if(sliderNext > count) {
+				sliderNext=1;
+				sliderAct=1;
+			};
+			$('#slider > div').fadeOut(300);
+			$('#slider > div#slide' + sliderNext).fadeIn(300);
+
+			sliderAct = sliderNext;
+			sliderNext = sliderNext+1;
+		}, 1000);
+	};
+
+	function stopSlider(){
+		window.clearInterval(loop);
+	}
+
+	$('#slider').hover(function(){ stopSlider(); }, function(){ startSlider(); });
+
+	$('.circle').each(function(){
+	   console.log($(this));
+	});
+});
