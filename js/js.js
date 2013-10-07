d472237ac44232eab6fd28d5356af8a6b664a75a
cdmngz <crlsdmngz3@gmail.com>
2013-10-07 00:22:17 -0300

    <a> tag's buttons
    Making it work... But not 100%

--- js/js.js
+++ js/js.js
@@ -2,32 +2,36 @@ $(function(){
 	var sliderAct=1;
 	var sliderNext=2;
 
-	$('#slider > div#slide1').fadeIn(300);
+	$("#slider > div#slide1").fadeIn(300);
 	startSlider();
 
 	function startSlider(){
-		count = $('#slider > div').size();
+		count = $("#slider > div").size();
 
 		loop = setInterval(function(){
 			if(sliderNext > count) {
 				sliderNext=1;
 				sliderAct=1;
 			};
-			$('#slider > div').fadeOut(300);
-			$('#slider > div#slide' + sliderNext).fadeIn(300);
+			$("#slider > div").fadeOut(300);
+			$("#slider > div#slide" + sliderNext).fadeIn(300);
 
 			sliderAct = sliderNext;
 			sliderNext = sliderNext+1;
-		}, 1000);
+		}, 2000);
 	};
 
 	function stopSlider(){
 		window.clearInterval(loop);
 	}
 
-	$('#slider').hover(function(){ stopSlider(); }, function(){ startSlider(); });
-
-	$('.circle').each(function(){
-	   console.log($(this));
+	$("#slider").hover(function(){ stopSlider(); }, function(){ startSlider(); });
+	
+	$( ".circle" ).click(function(){
+        var chi = $(this).index();
+        var chi = chi -2;
+		$("#slider > div").fadeOut(300);	
+		$("#slider > div#slide" + chi).fadeIn(300);
 	});
+
 });
